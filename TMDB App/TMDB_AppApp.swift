1ae939944ab36d4539f744f9b63734c87d034c67
onflyer <salkaorama@gmail.com>
2023-10-21 17:30:44 +0200

    Initial Commit

---
+++ TMDB App/TMDB_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_AppApp.swift
+//  TMDB App
+//
+//  Created by Aleksandar Milidrag on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct TMDB_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
