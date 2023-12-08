59aa162c9a051dd9a4a4090f6da41914b4733d2a
onflyer <salkaorama@gmail.com>
2023-12-08 16:04:36 +0100

    reverted to not using loading in home screen

--- TMDB App/Screens/MovieHomeScreen.swift
+++ TMDB App/Screens/MovieHomeScreen.swift
@@ -14,51 +14,39 @@ struct MovieHomeScreen: View {
         NavigationStack {
             ScrollView {
                 VStack {
-                    if viewModel.isLoading {
-                       CarouselPosterProgressView()
-                    } else {
-                        MoviePosterCarouselView(title: "Now Playing", movie: viewModel.nowPlayingMovies)
-                    }
-                    if viewModel.isLoading {
-                        CarouselBackdropProgressView()
-                    } else {
-                        MovieBackdropCarouselView(title: "Upcoming", movie: viewModel.upcomingMovies)
-                    }
-                    if viewModel.isLoading {
-                        CarouselBackdropProgressView()
-                    } else {
-                        MovieBackdropCarouselView(title: "Top Rated", movie: viewModel.topRatedMovies)
-                    }
-                    if viewModel.isLoading {
-                        CarouselBackdropProgressView()
-                    } else {
-                        MovieBackdropCarouselView(title: "Popular", movie: viewModel.popularMovies)
-                    }
+                    MoviePosterCarouselView(title: "Now Playing", movie: viewModel.nowPlayingMovies)
+                    
+                    MovieBackdropCarouselView(title: "Upcoming", movie: viewModel.upcomingMovies)
+                    
+                    MovieBackdropCarouselView(title: "Top Rated", movie: viewModel.topRatedMovies)
+                    
+                    MovieBackdropCarouselView(title: "Popular", movie: viewModel.popularMovies)
                 }
-            }
-            .scrollIndicators(.hidden)
-            .task {
-                await loadNowPlayingMovies()
-                await loadUpcomingMovies()
-                await loadTopRatedMovies()
-                await loadPopularMovies()
             }
             .navigationTitle("TMDB App")
-            .alert(viewModel.alert?.title ?? "Error", isPresented: Binding(projectedValue: $viewModel.showAlert)) {
-                viewModel.alert?.getButtonsForAlert
-            } message: {
-                if let subtitle = viewModel.alert?.subtitle {
-                    Text(subtitle)
-                }
-            }
-
-            
         }
+        .scrollIndicators(.hidden)
+        .task {
+            await loadNowPlayingMovies()
+            await loadUpcomingMovies()
+            await loadTopRatedMovies()
+            await loadPopularMovies()
+        }
+        
+        .alert(viewModel.alert?.title ?? "Error", isPresented: Binding(projectedValue: $viewModel.showAlert)) {
+            viewModel.alert?.getButtonsForAlert
+        } message: {
+            if let subtitle = viewModel.alert?.subtitle {
+                Text(subtitle)
+            }
+        }
+        
     }
     
+    
     func loadNowPlayingMovies() async {
-        viewModel.isLoading = true
-       
+        
+        
         do {
             try await viewModel.fetchNowPlayingMovies()
         } catch {
@@ -72,38 +60,37 @@ struct MovieHomeScreen: View {
             })
             print(error)
         }
-        viewModel.isLoading = false
+        
         
     }
     
     func loadUpcomingMovies() async {
-        viewModel.isLoading = true
+        
         do {
             try await viewModel.fetchUpcomingMovies()
         } catch {
             print(error)
         }
-        viewModel.isLoading = false
+        
     }
     
     func loadTopRatedMovies() async {
-        viewModel.isLoading = true
+        
         do {
             try await viewModel.fetchTopRatedMovies()
         } catch {
             print(error)
         }
-        viewModel.isLoading = false
+        
     }
     
     func loadPopularMovies() async {
-        viewModel.isLoading = true
+        
         do {
             try await viewModel.fetchPopularMovies()
         } catch {
             print(error)
         }
-        viewModel.isLoading = false
     }
 }
 
