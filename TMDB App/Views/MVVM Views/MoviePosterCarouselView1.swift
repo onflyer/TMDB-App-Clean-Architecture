c112b25fd4f18b040bda276f6803ef89ead16a94
onflyer <salkaorama@gmail.com>
2023-12-16 17:18:04 +0100

    minor update

---
+++ TMDB App/Views/MVVM Views/MoviePosterCarouselView1.swift
@@ -0,0 +1,18 @@
+//
+//  MoviePosterCarouselView1.swift
+//  TMDB App
+//
+//  Created by Aleksandar Milidrag on 12/16/23.
+//
+
+import SwiftUI
+
+struct MoviePosterCarouselView1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoviePosterCarouselView1()
+}
