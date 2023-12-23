a0dbf674a105f4d8332c853995818a491b335a6b
onflyer <salkaorama@gmail.com>
2023-12-23 12:44:08 +0100

    added add to favorites object

---
+++ TMDB App/Models/AddFavorite.swift
@@ -0,0 +1,20 @@
+//
+//  AddFavorite.swift
+//  TMDB App
+//
+//  Created by Aleksandar Milidrag on 12/23/23.
+//
+
+import Foundation
+
+struct AddFavoriteMovie: Codable {
+    let mediaType: String
+    let mediaID: Int
+    let favorite: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case mediaType = "media_type"
+        case mediaID = "media_id"
+        case favorite
+    }
+}
