fdf2297614aec8fc1b7eca21c78d191d92ac37f2
onflyer <salkaorama@gmail.com>
2024-03-14 19:23:23 +0100

    added now playing movies request impl.

--- TMDB Clean Architecture/Data/DataSource/Remote/MovieDataSource.swift
+++ TMDB Clean Architecture/Data/DataSource/Remote/MovieDataSource.swift
@@ -11,21 +11,40 @@ import Foundation
 // MARK: - Protocol -
 
 protocol MovieDataSource {
-    func getMovies() async throws -> [MovieDTO]
+    func getNowPlayingMovies() async throws -> [MovieDTO]
+    func getUpcomingMovies() async throws -> [MovieDTO]
+    func getTopRatedMovies() async throws -> [MovieDTO]
+    func getPopularMovies() async throws -> [MovieDTO]
 }
 
 // MARK: - Implementation -
 
 class DefaultMovieDataSource: MovieDataSource {
+    
+    
     private let requestManager: RequestManager
+    
     init(requestManager: RequestManager) {
         self.requestManager = requestManager
     }
     
-    func getMovies() async throws -> [MovieDTO] {
-        return []
-//        let request = CharactersRequest.getCharacters(offset: offset, searchKey: searchKey)
-//        let response: CharactersResponse = try await requestManager.makeRequest(with: request)
-//        return response.data
+    func getNowPlayingMovies() async throws -> [MovieDTO] {
+        let request = MoviesRequest.getNowPlayingMovies
+        let response: MovieListDTO = try await requestManager.makeRequest(with: request)
+        if let unwrapedResponse = response.results {
+            return unwrapedResponse
+        }
+    }
+    
+    func getUpcomingMovies() async throws -> [MovieDTO] {
+        <#code#>
+    }
+    
+    func getTopRatedMovies() async throws -> [MovieDTO] {
+        <#code#>
+    }
+    
+    func getPopularMovies() async throws -> [MovieDTO] {
+        <#code#>
     }
 }
