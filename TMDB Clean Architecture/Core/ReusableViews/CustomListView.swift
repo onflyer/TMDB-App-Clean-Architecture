6c9778255b1f866d401ebccc0dd1b976fe7801af
onflyer <salkaorama@gmail.com>
2024-03-18 13:55:23 +0100

    added customListView

---
+++ TMDB Clean Architecture/Core/ReusableViews/CustomListView.swift
@@ -0,0 +1,48 @@
+//
+//  CustomListView.swift
+//  TMDB App
+//
+//  Created by Aleksandar Milidrag on 3/18/24.
+//
+
+import SwiftUI
+
+struct CustomListView<Items, Destination, ItemView>: View
+where Items: Identifiable, Items: Equatable, Destination: View, ItemView: View {
+    
+    let items: [Items]
+    let navigationDestination: (Items) -> Destination
+    let itemView: (Items) -> ItemView
+    
+    init(items: [Items],
+         @ViewBuilder navigationDestination: @escaping (Items) -> Destination,
+         @ViewBuilder itemView: @escaping (Items) -> ItemView) {
+        self.items = items
+        self.navigationDestination = navigationDestination
+        self.itemView = itemView
+    }
+    
+    var body: some View {
+        List {
+            ForEach(items) { item in
+                ZStack(
+                    content: {
+                        itemView(item)
+                        NavigationLink(
+                            destination: navigationDestination(item)
+                                .toolbar(.hidden, for: .tabBar)
+                        ) {
+                            EmptyView()
+                        }
+                        .opacity(0)
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                )//: ZStack
+                .listRowSeparator(.hidden)
+                .listRowInsets(EdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0))
+            }//: ForEach
+        }//: List
+        .listStyle(PlainListStyle())
+        .scrollIndicators(.hidden)
+    }
+}
