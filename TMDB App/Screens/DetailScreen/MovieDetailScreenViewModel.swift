1caaa7e612c0f6569ae8a8399df5248417bb114c
onflyer <salkaorama@gmail.com>
2024-03-20 12:33:27 +0100

    removed dependancy packages

--- TMDB App/Screens/DetailScreen/MovieDetailScreenViewModel.swift
+++ TMDB App/Screens/DetailScreen/MovieDetailScreenViewModel.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import Dependencies
+
 
 @MainActor
 final class MovieDetailScreenViewModel: ObservableObject {
--- TMDB App/Screens/SearchScreen/MovieSearchScreenViewModel.swift
+++ TMDB App/Screens/SearchScreen/MovieSearchScreenViewModel.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import Dependencies
+
 
 @MainActor
 final class MovieSearchScreenViewModel: ObservableObject {
