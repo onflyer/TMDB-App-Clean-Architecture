9d835d181ed50eb29262491e3b6a40c3820ab844
onflyer <salkaorama@gmail.com>
2024-03-23 21:46:17 +0100

    added core data stack
    added data model and persistent container setup

---
+++ TMDB Clean Architecture/Data/DataSource/Local/CoreDataStack.swift
@@ -0,0 +1,72 @@
+//
+//  CoreDataStack.swift
+//  TMDB App
+//
+//  Created by Aleksandar Milidrag on 3/23/24.
+//
+
+import Foundation
+import CoreData
+
+open class CoreDataStack {
+    public static let modelName = "TMDBAppDataModel"
+    public static let model: NSManagedObjectModel = {
+        // swiftlint:disable force_unwrapping
+        let modelURL = Bundle.main.url(forResource: modelName, withExtension: "momd")!
+        return NSManagedObjectModel(contentsOf: modelURL)!
+        // swiftlint:enable force_unwrapping
+    }()
+    
+    public init() {
+    }
+    
+    public lazy var mainContext: NSManagedObjectContext = {
+        return self.storeContainer.viewContext
+    }()
+    
+    public lazy var storeContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: CoreDataStack.modelName, managedObjectModel: CoreDataStack.model)
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    public func newDerivedContext() -> NSManagedObjectContext {
+        let context = storeContainer.newBackgroundContext()
+        return context
+    }
+    
+    public func saveContext() {
+        saveContext(mainContext)
+    }
+    
+    public func saveContext(_ context: NSManagedObjectContext) {
+        if context != mainContext {
+            saveDerivedContext(context)
+            return
+        }
+        
+        context.perform {
+            do {
+                try context.save()
+            } catch let error as NSError {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+    
+    public func saveDerivedContext(_ context: NSManagedObjectContext) {
+        context.perform {
+            do {
+                try context.save()
+            } catch let error as NSError {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+            
+            self.saveContext(self.mainContext)
+        }
+    }
+}
