7c938837784362d1d3ecce3d04c68e93518e9b8f
onflyer <salkaorama@gmail.com>
2024-03-30 15:46:28 +0100

    Update CoreDataDTO+Extension.swift

--- TMDB Clean Architecture/Data/DTO Models/CoreDataDTO+Extension.swift
+++ TMDB Clean Architecture/Data/DTO Models/CoreDataDTO+Extension.swift
@@ -11,30 +11,18 @@ import CoreData
 extension CoreDataDTO {
     
    public var id: Int? {
-       get {
-           Int(id_)
-       }
-       set {
-           id_ = Int64(newValue ?? 0)
-       }
+       get { Int(id_) }
+       set { id_ = Int64(newValue ?? 0)}
     }
     
     var title: String? {
-        get {
-            title_
-        }
-        set {
-            title_ = newValue
-        }
+        get { title_}
+        set { title_ = newValue }
     }
     
     var posterPath: String? {
-        get {
-            posterPath_
-        }
-        set {
-            posterPath_ = newValue
-        }
+        get { posterPath_}
+        set { posterPath_ = newValue}
     }
     
     convenience init(id: Int, title: String, posterPath: String, context: NSManagedObjectContext) {
