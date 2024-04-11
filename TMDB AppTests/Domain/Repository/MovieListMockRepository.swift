f2a931e76c1bc5acc88d6614cd86184260a3525b
onflyer <salkaorama@gmail.com>
2024-04-11 21:12:43 +0200

    added mock movieListReposotory

---
+++ TMDB AppTests/Domain/Repository/MovieListMockRepository.swift
@@ -0,0 +1,51 @@
+//
+//  MovieListMockRepository.swift
+//  TMDB AppTests
+//
+//  Created by Aleksandar Milidrag on 4/11/24.
+//
+
+import Foundation
+@testable import TMDB_App
+
+
+class MovieListMockRepository: MovieListRepository {
+    func getNowPlayingMovies(page: Int) async -> Result<[MovieEntity], AppError> {
+        
+            let response = [MovieEntity(id: 1, posterPath: "testPoster1", coreDataTitle: "title1"),
+                            MovieEntity(id: 2, posterPath: "testPoster2", coreDataTitle: "title2"),
+                            MovieEntity(id: 3, posterPath: "testPoster3", coreDataTitle: "title3")]
+            return .success(response)
+    }
+    
+    func getUpcomingMovies(page: Int) async -> Result<[MovieEntity], AppError> {
+        let response = [MovieEntity(id: 1, posterPath: "testPoster1", coreDataTitle: "title1"),
+                        MovieEntity(id: 2, posterPath: "testPoster2", coreDataTitle: "title2"),
+                        MovieEntity(id: 3, posterPath: "testPoster3", coreDataTitle: "title3")
+        ]
+        return .success(response)
+    }
+    
+    func getTopRatedMovies(page: Int) async -> Result<[MovieEntity], AppError> {
+        let response = [MovieEntity(id: 1, posterPath: "testPoster1", coreDataTitle: "title1"),
+                        MovieEntity(id: 2, posterPath: "testPoster2", coreDataTitle: "title2"),
+                        MovieEntity(id: 3, posterPath: "testPoster3", coreDataTitle: "title3")
+        ]
+        return .success(response)
+    }
+    
+    func getPopularMovies(page: Int) async -> Result<[MovieEntity], AppError> {
+        let response = [MovieEntity(id: 1, posterPath: "testPoster1", coreDataTitle: "title1"),
+                        MovieEntity(id: 2, posterPath: "testPoster2", coreDataTitle: "title2"),
+                        MovieEntity(id: 3, posterPath: "testPoster3", coreDataTitle: "title3")
+        ]
+        return .success(response)
+    }
+    
+    func getMovieById(movieId: Int) async -> Result<SingleMovieEntity?, AppError> {
+        let movie = SingleMovieEntity.dummySingleMovie()
+        return .success(movie)
+    }
+    
+    
+}
---
+++ TMDB AppTests/Domain/UseCase/GetNowPlayingMoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_AppTests.swift
+//  TMDB AppTests
+//
+//  Created by Aleksandar Milidrag on 4/11/24.
+//
+
+import XCTest
+@testable import TMDB_App
+
+final class GetNowPlayingMoviesTests: XCTestCase {
+
+   
+
+}
+
+
