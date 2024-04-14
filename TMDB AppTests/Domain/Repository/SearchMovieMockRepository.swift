b5e6d1795f507e2f2dbb6eb915cf525bb1ca1947
onflyer <salkaorama@gmail.com>
2024-04-14 21:22:28 +0200

    ..

--- TMDB AppTests/Domain/Repository/SearchMovieMockRepository.swift
+++ TMDB AppTests/Domain/Repository/SearchMovieMockRepository.swift
@@ -9,12 +9,25 @@ import Foundation
 @testable import TMDB_App
 
 class SearchMovieMockRepository: SearchMovieRepository {
-    var movies: [MovieEntity]?
-    var error: AppError?
-    
+        
     func searchMovie(query: String) async throws -> [MovieEntity] {
-        return []
+        try await getMovies()
     }
     
     
+    //MARK: HELPERS
+    
+    var movies: [MovieEntity]?
+    var error: AppError?
+    
+    func getMovies() async throws -> [MovieEntity] {
+        if let error = error {
+            throw error
+        }
+        if let movies = movies {
+            return movies
+        }
+        fatalError("MovieListMockRepository not properly set up")
+    }
+
 }
---
+++ TMDB AppTests/Domain/UseCase/SearchMovieUseCaseTests.swift
@@ -0,0 +1,34 @@
+//
+//  SearchMovieUseCaseTests.swift
+//  TMDB AppTests
+//
+//  Created by Aleksandar Milidrag on 4/14/24.
+//
+
+import XCTest
+@testable import TMDB_App
+
+class SearchMovieUseCaseTests: XCTestCase {
+    
+    func test_searchMovieUseCaseSuccess() async {
+        let mock = SearchMovieMockRepository()
+        let expectedResponse = [MovieEntity(id: 1, posterPath: "testPoster1", coreDataTitle: "title1"),
+                                MovieEntity(id: 2, posterPath: "testPoster2", coreDataTitle: "title2"),
+                                MovieEntity(id: 3, posterPath: "testPoster3", coreDataTitle: "title3")]
+        mock.movies = expectedResponse
+        let sut = SearchMovieUseCaseImpl(repository: mock)
+        
+        do {
+            let result = try await sut.execute(query: "title")
+            XCTAssertEqual(result.count, 3)
+            XCTAssertTrue(result.contains(where: { $0.id == 1 })) // John Doe
+            XCTAssertTrue(result.contains(where: { $0.id == 3 }))
+        } catch  {
+            print(error)
+        }
+        
+        
+        
+    }
+}
+
